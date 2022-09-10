7d4bbdbd8c706b4c375e499f7c873fa2f749b108
cebbz <sebastien.capron@gmail.com>
2022-09-10 16:32:00 +0200

    Store & shopping cart layout

--- e-commerce-front/src/App.vue
+++ e-commerce-front/src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import TheHeader from './components/Header.vue';
 import TheFooter from './components/Footer.vue';
-import Shop from './components/Shop.vue';
-import Cart from './components/Cart.vue';
+import Shop from './components/Shop/Shop.vue';
+import Cart from './components/Cart/Cart.vue';
 </script>
   
 <template>
   <div class="app-container">
-    <TheHeader class="header b1" />
-    <Shop class="shop b2" />
-    <Cart class="cart b3" />
-    <TheFooter class="footer b4" />
+    <TheHeader class="header" />
+    <Shop class="shop" />
+    <Cart class="cart" />
+    <TheFooter class="footer" />
   </div>
 </template>
   
