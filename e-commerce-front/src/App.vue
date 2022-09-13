9adeddc962be237cc99eef1a736eb71019913895
cebbz <sebastien.capron@gmail.com>
2022-09-13 12:18:17 +0200

    Preparation for filtering products.

--- e-commerce-front/src/App.vue
+++ e-commerce-front/src/App.vue
@@ -3,13 +3,15 @@ import TheHeader from './components/Header.vue';
 import TheFooter from './components/Footer.vue';
 import Shop from './components/Shop/Shop.vue';
 import Cart from './components/Cart/Cart.vue';
-
 import data from './data/product';
-import { DEFAULT_FILTERS } from './data/filter';
-
 import { computed, reactive } from 'vue';
-import type { ProductInterface, ProductCartInterface, FiltersInterface } from './interfaces';
-
+import type {
+  FiltersInterface,
+  ProductCartInterface,
+  ProductInterface,
+  FilterUpdate,
+} from './interfaces';
+import { DEFAULT_FILTERS } from './data/filters';
 
 const state = reactive<{
   products: ProductInterface[];
@@ -18,7 +20,7 @@ const state = reactive<{
 }>({
   products: data,
   cart: [],
-  filters: DEFAULT_FILTERS,
+  filters: { ...DEFAULT_FILTERS },
 });
 
 function addProductToCart(productId: number): void {
@@ -46,7 +48,37 @@ function removeProductFromCart(productId: number): void {
   }
 }
 
+function updateFilter(filterUpdate: FilterUpdate) {
+  if (filterUpdate.search !== undefined) {
+    state.filters.search = filterUpdate.search;
+  } else if (filterUpdate.priceRange) {
+    state.filters.priceRange = filterUpdate.priceRange;
+  } else if (filterUpdate.category) {
+    state.filters.category = filterUpdate.category;
+  } else {
+    state.filters = { ...DEFAULT_FILTERS };
+  }
+}
+
 const cartEmpty = computed(() => state.cart.length === 0);
+
+const filteredProducts = computed(() => {
+  return state.products.filter((product) => {
+    if (
+      product.title
+        .toLocaleLowerCase()
+        .startsWith(state.filters.search.toLocaleLowerCase()) &&
+      product.price >= state.filters.priceRange[0] &&
+      product.price <= state.filters.priceRange[1] &&
+      (product.category === state.filters.category ||
+        state.filters.category === 'all')
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+});
 </script>
   
 <template>
@@ -54,7 +86,8 @@ const cartEmpty = computed(() => state.cart.length === 0);
     gridEmpty: cartEmpty,
   }">
     <TheHeader class="header" />
-    <Shop :products="state.products" @add-product-to-cart="addProductToCart" class="shop" />
+    <Shop @update-filter="updateFilter" :products="filteredProducts" @add-product-to-cart="addProductToCart"
+      class="shop" />
     <Cart v-if="!cartEmpty" :cart="state.cart" class="cart" @remove-product-from-cart="removeProductFromCart" />
     <TheFooter class="footer" />
   </div>
@@ -95,3 +128,4 @@ const cartEmpty = computed(() => state.cart.length === 0);
   grid-area: footer;
 }
 </style>
+  
