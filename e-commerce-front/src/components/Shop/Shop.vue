f58264ff581a6c647c85320f18317ccd733e226a
cebbz <sebastien.capron@gmail.com>
2022-09-13 18:19:59 +0200

    select categroy

--- e-commerce-front/src/components/Shop/Shop.vue
+++ e-commerce-front/src/components/Shop/Shop.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import type { ProductInterface, FilterUpdate, FiltersInterface } from '@/interfaces';
-import ShopProductList from "./ShopProductList.vue";
-import ShopFilters from "./ShopFilters.vue";
+import type {
+    FiltersInterface,
+    ProductInterface,
+    FilterUpdate,
+} from '../../interfaces';
+import ShopProductList from './ShopProductList.vue';
+import ShopFilters from './ShopFilters.vue';
 
 defineProps<{
     products: ProductInterface[];
@@ -9,19 +13,20 @@ defineProps<{
 }>();
 
 const emit = defineEmits<{
-    (event: 'add-product-to-cart', productId: number): void;
-    (event: 'update-filter', updateFilter: FilterUpdate): void;
+    (e: 'addProductToCart', productId: number): void;
+    (e: 'updateFilter', updateFilter: FilterUpdate): void;
 }>();
 </script>
-
+    
 <template>
     <div class="d-flex flex-row">
-        <ShopFilters :filters="filters" @update-filter="emit('update-filter', $event)" class="shop-filter" />
-        <ShopProductList :products="products" @add-product-to-cart="emit('add-product-to-cart', $event)"
-            class="flex-fill" />
+        <ShopFilters :filters="filters" :nbr-of-products="products.length" @update-filter="emit('updateFilter', $event)"
+            class="shop-filter" />
+        <ShopProductList class="flex-fill" @add-product-to-cart="emit('addProductToCart', $event)"
+            :products="products" />
     </div>
 </template>
-
+    
 <style lang="scss" scoped>
 .shop-filter {
     flex: 0 0 200px;
--- e-commerce-front/src/components/Shop/ShopFilters.vue
+++ e-commerce-front/src/components/Shop/ShopFilters.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-import type { FiltersInterface, FilterUpdate } from '@/interfaces';
+import type {
+    FiltersInterface,
+    FilterUpdate,
+    Category,
+} from '../../interfaces';
 defineProps<{
     filters: FiltersInterface;
+    nbrOfProducts: number;
 }>();
 const emit = defineEmits<{
     (e: 'updateFilter', filterUpdate: FilterUpdate): void;
@@ -21,18 +26,49 @@ const emit = defineEmits<{
             <div class="mb-5"
                 v-for="priceRange of ([[0, 10000], [800, 1000], [1000, 1500], [1500, 2000], [2000, 10000]] as [number, number][])">
                 <input :checked="filters.priceRange[0] === priceRange[0]" type="radio"
-                    @input="emit('updateFilter', { priceRange })" name="priceRange" :id="priceRange[0].toString()" />
-                <label :for="priceRange[0].toString()">
+                    @input="emit('updateFilter', { priceRange })" name="priceRange" :id="priceRange[0] + ''" />
+                <label :for="priceRange[0] + ''">
                     {{
-                    priceRange[0] === 0 ? 'Tous les prix': priceRange[0] === 2000 ? 'Plus de 2000€' : `Entre
-                    ${priceRange[0]}€ et ${priceRange[1]}€`
+                    priceRange[0] === 0
+                    ? 'Tous les prix'
+                    : priceRange[0] === 2000
+                    ? 'Plus de 2000€'
+                    : `Entre ${priceRange[0]}€ et ${priceRange[1]}€`
                     }}
                 </label>
             </div>
         </section>
+        <section class="mb-20 flex-fill">
+            <h3 class="mb-10">Trier par categories</h3>
+            <p class="category" :class="{ selected: filters.category === category }"
+                v-for="category in (['all', 'desktop', 'gamer', 'streaming'] as Category[])"
+                @click="emit('updateFilter', { category })">
+                {{ category }}
+            </p>
+        </section>
+        <small class="mb-5">
+            Nombre de résultats:
+            <strong>{{ nbrOfProducts }}</strong>
+        </small>
+        <button class="btn btn-danger" @click="emit('updateFilter', {})">
+            Supprimer les filtres
+        </button>
     </div>
 </template>
     
 <style lang="scss" scoped>
+.category {
+    font-size: 14px;
+    line-height: 18px;
+    cursor: pointer;
 
+    &:hover {
+        text-decoration: underline;
+    }
+}
+
+.selected {
+    font-weight: bold;
+    text-decoration: underline;
+}
 </style>
